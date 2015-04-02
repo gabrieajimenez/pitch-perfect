9d79f2ee36986b8902f6a2e33fc50cba8531a882
gabsjmz <info@gabrielajimenez.com>
2015-04-01 18:26:18 -0600

    Committed by Udacity

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by gabriela jimenez rocha on 01/04/15.
+//  Copyright (c) 2015 GabrielaJimenez. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
